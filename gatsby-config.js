22fd427046b0e62d029a0647ceebc9668975551b
mattrhummel <matt@matthummel.com>
2023-02-27 17:05:12 -0500

    new update

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,13 @@ module.exports = {
     title: `Project Starter`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-netlify-cms", "gatsby-plugin-postcss", "gatsby-plugin-google-gtag", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
+  plugins: [
+    'gatsby-plugin-postcss',
+    "gatsby-plugin-netlify-cms",
+    "gatsby-plugin-postcss", 
+    "gatsby-plugin-google-gtag", 
+    "gatsby-plugin-image", 
+    "gatsby-plugin-sitemap", {
     resolve: 'gatsby-plugin-manifest',
     options: {
       "icon": "src/images/icon.png"
