4cde04d75edba4ac38f71b2ed1990247c9ac100c
mattrhummel <matt@matthummel.com>
2023-02-27 19:01:59 -0500

    fix build error

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@
 module.exports = {
   siteMetadata: {
     title: `Project Starter`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://matthummelweb.dev`
   },
   plugins: [
     'gatsby-plugin-postcss',
